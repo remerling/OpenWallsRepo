e25f3b66c2ab5d35d4e10abdf0b6e17530e53a35
remerling <52681011+remerling@users.noreply.github.com>
2020-07-20 21:57:56 -0400

    Progress on CFrame
    CFrame movement script progress and a diagram

--- emerlingTestScript.lua
+++ emerlingTestScript.lua
@@ -1,7 +1,16 @@
-print("hhhhhhhhhhhhhhhh")
 local part2 = game.Workspace.MeshToMove2
 local wall2 = game.Workspace.Wall2
 
+local px,py,pz,xx,yx,zx,xy,yy,zy,xz,yz,zz = wall2.CFrame:components()
+local Position=Vector3.new(px,py,pz)
+local Right=Vector3.new(xx,xy,xz)
+local Top=Vector3.new(yx,yy,yz)
+local Back=Vector3.new(zx,zy,zz)
+
+print(Position)
+print(Right)
+print(Top)
+print(Back)
 
 --wall2 = CFrame.Angles(math.rad(180),math.rad(180),math.rad(90))
 
@@ -11,10 +20,16 @@ local wall2 = game.Workspace.Wall2
 
 for i = 0, 1, .01 do
 	wait()
-		--part2.CFrame = part2.CFrame:Lerp((wall2.CFrame),i)
-		--part2.CFrame = part2.CFrame:Lerp((CFrame:GetComponents(wall2))* (math.rad(180),math.rad(180),math.rad(90)),i)
-		--part2.CFrame = CFrame.new(newc.CFrame.Position, newc.CFrame.LookVector)
-		--part2.CFrame = CFrame.new(wall2.Position, math.rad(180),math.rad(180),math.rad(90)))		
-		--part2.CFrame = CFrame.new(newc.CFrame.Position)
-		part2.CFrame = CFrame.new(wall2.CFrame:GetComponents())
+	--part2.CFrame = part2.CFrame:Lerp((wall2.CFrame),i)
+	--part2.CFrame = part2.CFrame:Lerp((CFrame:GetComponents(wall2))* (math.rad(180),math.rad(180),math.rad(90)),i)
+	--part2.CFrame = CFrame.new(newc.CFrame.Position, newc.CFrame.LookVector)
+	--part2.CFrame = CFrame.new(wall2.Position, math.rad(180),math.rad(180),math.rad(90))	
+	--part2.CFrame = CFrame.new(newc.CFrame.Position)
+	--part2.CFrame = CFrame.new(wall2.CFrame:GetComponents())
+	--part2.CFrame = CFrame.Angles(math.rad(180),math.rad(180),math.rad(90))
+	--part2.CFrame = CFrame.new(Position)
+	--part2.CFrame = CFrame.fromOrientation(part2.CFrame:ToOrientation ( ))
+	--part2.CFrame = CFrame.fromEulerAnglesXYZ ( zx, zy, zz )
+	part2.CFrame = CFrame.new(px,py,pz,xx,yx,zx,xy,yy,zy,xz,yz,zz)
+
 end
